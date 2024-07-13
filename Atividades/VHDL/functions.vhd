cdb7e6c6d10029ef5cee4bf0dd36cdba0896d484
Leonardo Santos <81315155+Je-Leo-AS@users.noreply.github.com>
2024-07-13 10:20:18 -0300

    teste da função pow()

--- Atividades/VHDL/functions.vhd
+++ Atividades/VHDL/functions.vhd
@@ -1,34 +1,45 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;  -- Biblioteca para operações aritméticas e conversões.
+use IEEE.NUMERIC_STD.ALL;
+
+package ComplexConstants is
+
+end package ComplexConstants;
 
 package Math_Package is
-    function std_logic_vector_to_integer(vec: std_logic_vector) return integer;
-    function pow(base: std_logic_vector) return std_logic_vector;
+  
+  constant n_bits_resolution : INTEGER := 9;
+  
+  constant max_value : INTEGER := 2**n_bits_resolution -1;
+  
+  subtype limited_integer is integer range - max_value to max_value;
+  
+  function pow(base: limited_integer) return integer;
+ 
+  type complex_number is record
+    real : limited_integer;
+    imag : limited_integer;
+  end record;
+
+  type complex_array is array (0 to 3) of complex_number;
+
+  constant coefficients : complex_array := (
+    (real => 1, imag => 1),
+    (real => 8, imag => -1),
+    (real => -3, imag => -1),
+    (real => 1, imag => 1)
+  );
+
 end Math_Package;
 
 package body Math_Package is
-    -- Função para converter std_logic_vector para integer
-    function std_logic_vector_to_integer(vec: std_logic_vector) return integer is
-        variable result: integer := 0;
-    begin
-        for i in vec'range loop
-            if vec(i) = '1' then
-                result := result + 2**(vec'length - 1 - i);
-            end if;
-        end loop;
-        return result;
-    end std_logic_vector_to_integer;
-
+    
     -- Função para calcular a potência
-    function pow(base: std_logic_vector) return std_logic_vector is
-        variable base_int: integer;
+    function pow(base: limited_integer) return integer is
         variable result_int: integer := 0;
-        variable result_vec: std_logic_vector(base'range);
+	variable base_int: limited_integer;
     begin
-        base_int := std_logic_vector_to_integer(base);
         result_int := base_int * base_int;
-        result_vec := std_logic_vector(to_unsigned(result_int, base'length - 1));
-        return result_vec;
+        return result_int;
     end pow;
 end Math_Package;
--- Atividades/VHDL/main_test.vhd
+++ Atividades/VHDL/main_test.vhd
@@ -6,28 +6,31 @@ use work.Math_Package.all;
 
 entity test_power is 
     generic(
-        n_bits_resolution : INTEGER := 10 + 1;
         n_bits_overflow :INTEGER := 10;
         n_signals_used : INTEGER := 2;
         n_polygnos_degree : INTEGER := 2);
     port(
         reset : in std_logic;
         clk : in std_logic;
-        U : in std_logic_vector(n_bits_resolution downto 0);
-        U_out : out std_logic_vector(n_bits_resolution downto 0));
+        U : in signed(n_bits_resolution downto 0);
+	U_out_int : out integer;
+        U_out : out signed(n_bits_resolution downto 0));
 end test_power;
 
 architecture hardware of test_power is
-    signal u_unsigned : unsigned(n_bits_resolution downto 0) := (others => '0');
-
+   signal complex_signal_in : complex_number;  
+   signal u_int : limited_integer;
   begin
-    u_unsigned <= unsigned(U);
+    u_int <= to_integer(U);
     calcul_process: process(clk, reset)
+variable int_out : integer := 0; 
     begin
       if reset = '1' then
         U_out <= (others => '0'); 
       elsif rising_edge(clk) then
-         U_out <= std_logic_vector(pow(std_logic_vector(u_unsigned)));
+	int_out := pow(u_int);
+	U_out_int <= int_out;
+        U_out <= to_signed(u_int, n_bits_resolution + 1);
       end if;
     end process;
 end hardware;
