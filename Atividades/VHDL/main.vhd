7732d1e82f7274c174616e2b42389726d8cbfd7c
Leonardo Santos <81315155+Je-Leo-AS@users.noreply.github.com>
2024-07-14 18:37:51 -0300

    correções do arquivo principal

--- Atividades/VHDL/main.vhd
+++ Atividades/VHDL/main.vhd
@@ -1,47 +1,49 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 LIBRARY work;
 USE work.Math_Package.ALL;
 
 ENTITY DPD IS
   GENERIC (
     n_signals_used : INTEGER := 2;
-    n_polygnos_degree : INTEGER := 2;
+    n_polygnos_degree : INTEGER := 2
   );
   PORT (
     reset : IN STD_LOGIC;
     clk : IN STD_LOGIC;
     UR : IN signed(n_bits_resolution DOWNTO 0);
-    IR : IN signed(n_bits_resolution DOWNTO 0);
+    UI : IN signed(n_bits_resolution DOWNTO 0);
     UR_out : OUT signed(n_bits_resolution DOWNTO 0);
-    IR_out : OUT signed(n_bits_resolution DOWNTO 0));
+    UI_out : OUT signed(n_bits_resolution DOWNTO 0));
 END DPD;
 
 ARCHITECTURE hardware OF DPD IS
-  TYPE Array_signals IS ARRAY (0 TO n_signals_used) OF complex_number(n_bits_resolution DOWNTO 0);
-  TYPE Array_signals_overflow IS ARRAY (0 TO n_signals_used) OF complex_number_overflow(n_bits_resolution DOWNTO 0);
+  TYPE Array_signals IS ARRAY (0 TO n_signals_used) OF complex_number;
+  TYPE Array_signals_overflow IS ARRAY (0 TO n_signals_used) OF complex_number_overflow;
   TYPE Array_signals_powers IS ARRAY (0 TO n_polygnos_degree) OF Array_signals;
   TYPE Array_signals_powers_overflow IS ARRAY (0 TO n_polygnos_degree) OF Array_signals_overflow;
   SIGNAL U_signals : Array_signals := (OTHERS => (real => 0, imag => 0));
   SIGNAL U_signal_out : complex_number := (real => 0, imag => 0);
   SIGNAL U_signal_in : complex_number := (real => 0, imag => 0);
 BEGIN
-
   -- Processo que atualiza novos sinais a cada ciclo de clock
-  process_signal : PROCESS (reset, U_signals_out)
+  U_signal_in <= (real => to_integer(UR), imag => to_integer(UI));
+  process_signal : PROCESS (reset, U_signal_out)
+    VARIABLE count_signal : INTEGER RANGE 0 TO n_signals_used := 0;
   BEGIN
     IF reset = '1' THEN
       UR_out <= (OTHERS => '0');
-      IR_out <= (OTHERS => '0');
-      U_signals <= (OTHERS => (OTHERS => '0'));
+      UI_out <= (OTHERS => '0');
+      U_signals <= (OTHERS => (real => 0, imag => 0));
     ELSE
-      FOR i IN n_signals_used - 1 DOWNTO 1 :
-        U_signals(i) <= U_signals(i - 1)
+      FOR count_signal IN n_signals_used - 1 DOWNTO 1 LOOP
+        U_signals(count_signal) <= U_signals(count_signal - 1);
       END LOOP;
-      U_signal_in := (real => to_integer(UR), imag => to_integer(UI));
+      
       U_signals(0) <= U_signal_in;
-      UR_out <= to_signed(U_signals_out.real, UR_out'length);
-      UI_out <= to_signed(U_signals_out.imag, UI_out'length);
+      UR_out <= to_signed(U_signal_out.real, UR_out'length);
+      UI_out <= to_signed(U_signal_out.imag, UI_out'length);
     END IF;
   END PROCESS;
 
@@ -50,38 +52,38 @@ BEGIN
     VARIABLE sum : Array_signals := (OTHERS => (real => 0, imag => 0));
     VARIABLE potencias : Array_signals_powers := (OTHERS => (OTHERS => (real => 0, imag => 0)));
     VARIABLE multiplic : Array_signals_powers_overflow := (OTHERS => (OTHERS => (real => 0, imag => 0)));
-    VARIABLE count_signal : INTEGER RANGE (0 TO n_signals_used) := 0;
-    VARIABLE count_pot : INTEGER RANGE (0 TO n_polygnos_degree) := 0;
+    VARIABLE count_signal : INTEGER RANGE 0 TO n_signals_used - 1 := 0;
+    VARIABLE count_pot : INTEGER RANGE 0 TO n_polygnos_degree - 1:= 0;
+    VARIABLE index : INTEGER RANGE 0 TO n_polygnos_degree *  n_signals_used - 1 := 0;
     VARIABLE U_signals_out_temp : complex_number := (real => 0, imag => 0);
   BEGIN
     IF reset = '1' THEN
-      potencias : Array_signals_powers := (OTHERS => (OTHERS => (real => 0, imag => 0)));
+      potencias := (OTHERS => (OTHERS => (real => 0, imag => 0)));
       count_signal := 0;
       count_pot := 0;
-      U_signals_out <= 0;
+      U_signal_out <= (real => 0, imag => 0);
     ELSIF rising_edge(clk) THEN
-      potencias(count_pot + 1)(0).real <= potencias(count_pot)(0).real * potencias(count_pot)(0).real
-      potencias(count_pot + 1)(0).imag <= potencias(count_pot)(0).imag * potencias(count_pot)(0).imag
-      count_pot := count_pot + 1;
-      U_signals_out_temp := 0;
-      FOR j IN 0 TO n_signals_used - 1 :
-        multiplic(count_pot)(j).real := coefficients((j + count_pot) * 2).real * potencias(count_pot)(j).real - coefficients((j + count_pot) * 2).imag * potencias(count_pot)(j).imag
-        multiplic(count_pot)(j).imag := coefficients((j + count_pot) * 2).imag * potencias(count_pot)(j).real + coefficients((j + count_pot) * 2).real * potencias(count_pot)(j).imag
-        sum(count_pot).real := sum(count_pot).real + multiplic(j).real;
-        sum(count_pot).imag := sum(count_pot).imag + multiplic(j).imag;
+      potencias(count_pot + 1)(0).real := potencias(count_pot)(0).real * potencias(count_pot)(0).real;
+      potencias(count_pot + 1)(0).imag := potencias(count_pot)(0).imag * potencias(count_pot)(0).imag;
+      FOR j IN 0 TO n_signals_used - 1 LOOP
+	index := (j + count_pot) * n_polygnos_degree;
+        multiplic(count_pot)(j).real := coefficients(index).real * potencias(count_pot)(j).real - coefficients(index).imag * potencias(count_pot)(j).imag;
+        multiplic(count_pot)(j).imag := coefficients(index).imag * potencias(count_pot)(j).real + coefficients(index).real * potencias(count_pot)(j).imag;
+        sum(count_pot).real := sum(count_pot).real + multiplic(count_pot)(j).real;
+        sum(count_pot).imag := sum(count_pot).imag + multiplic(count_pot)(j).imag;
       END LOOP;
+      count_pot := count_pot + 1;
+      U_signals_out_temp := (real => 0, imag => 0);
       IF count_pot = n_polygnos_degree THEN
         count_pot := 0;
-        FOR k IN 0 TO n_signals_used :
-          U_signals_out_temp := U_signals_out_temp + sum(k);
+        FOR k IN 0 TO n_signals_used LOOP
+          U_signals_out_temp.real := U_signals_out_temp.real + sum(k).real;
+	  U_signals_out_temp.imag := U_signals_out_temp.imag + sum(k).imag;
         END LOOP;
-        FOR i IN n_polygnos_degree - 1 DOWNTO 1 :
-          potencias(i) <= potencias(i - 1)
+        FOR i IN n_polygnos_degree - 1 DOWNTO 1 LOOP
+          potencias(i) := potencias(i - 1);
         END LOOP;
-        U_signals_out <= U_signals_out_temp;
-      ELSE
-        U_signals_out <= U_signals_out;
-
+        U_signal_out <= U_signals_out_temp;
       END IF;
     END IF;
   END PROCESS;
--- Atividades/VHDL/main_test.vhd
+++ Atividades/VHDL/main_test.vhd
@@ -4,33 +4,88 @@ USE IEEE.NUMERIC_STD.ALL;
 LIBRARY work;
 USE work.Math_Package.ALL;
 
-ENTITY test_power IS
+ENTITY DPD IS
   GENERIC (
-    n_bits_overflow : INTEGER := 10;
     n_signals_used : INTEGER := 2;
-    n_polygnos_degree : INTEGER := 2);
+    n_polygnos_degree : INTEGER := 2
+  );
   PORT (
     reset : IN STD_LOGIC;
     clk : IN STD_LOGIC;
-    U : IN signed(n_bits_resolution DOWNTO 0);
-    U_out_int : OUT INTEGER;
-    U_out : OUT signed(n_bits_resolution DOWNTO 0));
-END test_power;
+    UR : IN signed(n_bits_resolution DOWNTO 0);
+    UI : IN signed(n_bits_resolution DOWNTO 0);
+    UR_out : OUT signed(n_bits_resolution DOWNTO 0);
+    UI_out : OUT signed(n_bits_resolution DOWNTO 0));
+END DPD;
 
-ARCHITECTURE hardware OF test_power IS
-  SIGNAL complex_signal_in : complex_number;
-  SIGNAL u_int : limited_integer;
+ARCHITECTURE hardware OF DPD IS
+  TYPE Array_signals IS ARRAY (0 TO n_signals_used) OF complex_number;
+  TYPE Array_signals_overflow IS ARRAY (0 TO n_signals_used) OF complex_number_overflow;
+  TYPE Array_signals_powers IS ARRAY (0 TO n_polygnos_degree) OF Array_signals;
+  TYPE Array_signals_powers_overflow IS ARRAY (0 TO n_polygnos_degree) OF Array_signals_overflow;
+  SIGNAL U_signals : Array_signals := (OTHERS => (real => 0, imag => 0));
+  SIGNAL U_signal_out : complex_number := (real => 0, imag => 0);
+  SIGNAL U_signal_in : complex_number := (real => 0, imag => 0);
 BEGIN
-  u_int <= to_integer(U);
-  calcul_process : PROCESS (clk, reset)
-    VARIABLE int_out : INTEGER := 0;
+  -- Processo que atualiza novos sinais a cada ciclo de clock
+  U_signal_in <= (real => to_integer(UR), imag => to_integer(UI));
+  process_signal : PROCESS (reset, U_signal_out)
+    VARIABLE count_signal : INTEGER RANGE 0 TO n_signals_used := 0;
   BEGIN
     IF reset = '1' THEN
-      U_out <= (OTHERS => '0');
-    ELSIF rising_edge(clk) THEN
-      int_out := u_int * u_int;
-      U_out_int <= int_out;
-      U_out <= to_signed(int_out, U_out'length);
+      UR_out <= (OTHERS => '0');
+      UI_out <= (OTHERS => '0');
+      U_signals <= (OTHERS => (real => 0, imag => 0));
+    ELSE
+      FOR count_signal IN n_signals_used - 1 DOWNTO 1 LOOP
+        U_signals(count_signal) <= U_signals(count_signal - 1);
+      END LOOP;
+      
+      U_signals(0) <= U_signal_in;
+      UR_out <= to_signed(U_signal_out.real, UR_out'length);
+      UI_out <= to_signed(U_signal_out.imag, UI_out'length);
     END IF;
   END PROCESS;
-END hardware;
+
+  -- processo que realiza os calculos e adiciona saida
+  calcul_process : PROCESS (clk, reset)
+    VARIABLE sum : Array_signals := (OTHERS => (real => 0, imag => 0));
+    VARIABLE potencias : Array_signals_powers := (OTHERS => (OTHERS => (real => 0, imag => 0)));
+    VARIABLE multiplic : Array_signals_powers_overflow := (OTHERS => (OTHERS => (real => 0, imag => 0)));
+    VARIABLE count_signal : INTEGER RANGE 0 TO n_signals_used - 1 := 0;
+    VARIABLE count_pot : INTEGER RANGE 0 TO n_polygnos_degree - 1:= 0;
+    VARIABLE index : INTEGER RANGE 0 TO n_polygnos_degree *  n_signals_used - 1 := 0;
+    VARIABLE U_signals_out_temp : complex_number := (real => 0, imag => 0);
+  BEGIN
+    IF reset = '1' THEN
+      potencias := (OTHERS => (OTHERS => (real => 0, imag => 0)));
+      count_signal := 0;
+      count_pot := 0;
+      U_signal_out <= (real => 0, imag => 0);
+    ELSIF rising_edge(clk) THEN
+      potencias(count_pot + 1)(0).real := potencias(count_pot)(0).real * potencias(count_pot)(0).real;
+      potencias(count_pot + 1)(0).imag := potencias(count_pot)(0).imag * potencias(count_pot)(0).imag;
+      FOR j IN 0 TO n_signals_used - 1 LOOP
+	index := (j + count_pot) * n_polygnos_degree;
+        multiplic(count_pot)(j).real := coefficients(index).real * potencias(count_pot)(j).real - coefficients(index).imag * potencias(count_pot)(j).imag;
+        multiplic(count_pot)(j).imag := coefficients(index).imag * potencias(count_pot)(j).real + coefficients(index).real * potencias(count_pot)(j).imag;
+        sum(count_pot).real := sum(count_pot).real + multiplic(count_pot)(j).real;
+        sum(count_pot).imag := sum(count_pot).imag + multiplic(count_pot)(j).imag;
+      END LOOP;
+      count_pot := count_pot + 1;
+      U_signals_out_temp := (real => 0, imag => 0);
+      IF count_pot = n_polygnos_degree THEN
+        count_pot := 0;
+        FOR k IN 0 TO n_signals_used LOOP
+          U_signals_out_temp.real := U_signals_out_temp.real + sum(k).real;
+	  U_signals_out_temp.imag := U_signals_out_temp.imag + sum(k).imag;
+        END LOOP;
+        FOR i IN n_polygnos_degree - 1 DOWNTO 1 LOOP
+          potencias(i) := potencias(i - 1);
+        END LOOP;
+        U_signal_out <= U_signals_out_temp;
+      END IF;
+    END IF;
+  END PROCESS;
+
+END ARCHITECTURE;
