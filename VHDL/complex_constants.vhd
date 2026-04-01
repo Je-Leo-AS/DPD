c8e8ad586c694183f821d3307260d7786c3b715d
Leonardo Santos <81315155+Je-Leo-AS@users.noreply.github.com>
2026-04-02 09:25:56 +1000

    fix

--- VHDL/complex_constants.vhd
+++ VHDL/complex_constants.vhd
@@ -3,9 +3,8 @@
     CONSTANT n_bits_resolution : INTEGER := 10 + 1;
     CONSTANT n_bits_overflow   : INTEGER := 10;
 
+    
     CONSTANT coefficients : coef_array_t := (
         ((reall => 1017, imag => -52), (reall => 177, imag => 201)),
         ((reall => -28, imag => 11), (reall => -39, imag => 1))
     );
-
-END PACKAGE;
