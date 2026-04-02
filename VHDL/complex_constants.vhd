2aa1f5e11db041e52e13411c35e668c9fad7eae2
Leonardo Santos <81315155+Je-Leo-AS@users.noreply.github.com>
2026-04-02 10:56:41 +1000

    ordens polinomiais truncadas

--- VHDL/complex_constants.vhd
+++ VHDL/complex_constants.vhd
@@ -1,11 +1,20 @@
-    CONSTANT n_signals_used    : INTEGER := 2 + 1;
-    CONSTANT max_poly_degree   : INTEGER := 5;
-    CONSTANT n_bits_resolution : INTEGER := 10 + 1;
-    CONSTANT n_bits_overflow   : INTEGER := 10;
-    
-        ((reall => 1020, imag => -15), (reall => 570, imag => -81), (reall => -583, imag => 252), (reall => -114, imag => 215), (reall => 735, imag => 178)),
-        ((reall => -116, imag => 8), (reall => -327, imag => 55), (reall => -53, imag => -84), (reall => 103, imag => -103), (reall => 315, imag => 12)),
-        ((reall => 87, imag => -4), (reall => -31, imag => 10), (reall => 369, imag => -21), (reall => -404, imag => -3), (reall => 36, imag => 111))
-    );
+  CONSTANT n_signals_used : INTEGER := 3;
+  CONSTANT max_poly_degree : INTEGER := 5;
+  CONSTANT n_bits_resolution : INTEGER := 11;
+  CONSTANT n_bits_overflow   : INTEGER := 10;
 
-END PACKAGE;
+  TYPE degree_array_t IS ARRAY (0 TO n_signals_used - 1) OF INTEGER;
+  CONSTANT poly_degree_per_delay : degree_array_t := (5, 3, 1);
+
+  TYPE complex_type IS RECORD
+    reall : INTEGER;
+    imag  : INTEGER;
+  END RECORD;
+
+  TYPE complex_array IS ARRAY (0 TO n_signals_used - 1, 0 TO n_polygnos_degree - 1) OF complex_type;
+
+  CONSTANT coefficients : complex_array := (
+    ((reall => 1022, imag => -20), (reall => 590, imag => -18), (reall => -849, imag => 138), (reall => 156, imag => 117), (reall => 737, imag => 289)),
+    ((reall => -122, imag => 15), (reall => -317, imag => 9), (reall => 267, imag => 0), (reall => 0, imag => 0), (reall => 0, imag => 0)),
+    ((reall => 92, imag => -2), (reall => 0, imag => 0), (reall => 0, imag => 0), (reall => 0, imag => 0), (reall => 0, imag => 0))
+  );
